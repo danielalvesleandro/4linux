490c75d1141d4d63bdbdc63faea7d0b53f91cad0
Ivan Ferro <iaferro@gmail.com>
2019-05-06 20:44:24 -0300

    Aula 8

--- aula_9/app.py
+++ aula_9/app.py
@@ -2,7 +2,14 @@ import threading
 
 import modules.banco as banco
 
-if __name__ '_main__':
-    pass
-
-
+if __name__ '__main__':             # 
+    usuario = input('Digite seu nick: ')
+    try:
+        f = threading.Thread(target=banco.select)
+        f.start()
+    except Exception as err:
+        print('Falha ao criar thread: {}'.format(err))
+    
+    while f.isAlive:
+        mensagem = input()
+        banco.cadastrar(usuario, mensagem)
